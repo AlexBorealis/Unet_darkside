7ef4c681f09eb0fdf9f257899a6b3e964df33f92
Alexandr Kozlov <93259323+AlexBorealis@users.noreply.github.com>
2025-01-15 17:54:41 +0700

    Add files via upload
    Update usefull scripts

---
+++ hyperparameters.py
@@ -0,0 +1,29 @@
+import os
+import warnings
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau, CSVLogger
+
+# Setting directory
+os.chdir('/workspace/darkside_test')
+
+# Suppressed warnings
+warnings.filterwarnings('ignore')
+
+# Names datasets
+name_training_dataset = "dark-side-train-data-part3"
+name_test_dataset = "dark-side-test-data-part-3"
+
+# Levels of clip
+low_clip = 5
+high_clip = 95
+
+# Callbacks variables
+tensorboard_logs_name = './logs_' + name_training_dataset
+csv_logger_name = 'training_log_unet_' + name_training_dataset + '.csv'
+
+# Callbacks
+model_checkpoint = ModelCheckpoint('./model/best_unet_model.{loss:.2f}.h5.keras')
+tensorboard = TensorBoard(log_dir= tensorboard_logs_name)
+reduce_on_plateau = ReduceLROnPlateau(monitor= 'loss', patience= 5)
+early_stopping = EarlyStopping(monitor= "loss", patience= 50, start_from_epoch= 50, min_delta= .01, verbose= 1)
+csv_logger = CSVLogger(csv_logger_name, separator= ",", append= True)
