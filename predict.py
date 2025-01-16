080449cca7e354f8359c01b227dbcf290e7edcea
Alexandr Kozlov <93259323+AlexBorealis@users.noreply.github.com>
2025-01-16 19:28:51 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ from network import *
 test_data = u.get_folders(name_test_dataset)
 
 # Reading testing file for predicting
-seismic_test = u.rescale_volume(u.get_cubes(name_test_dataset, test_data[4], 'seismic'), low= 5, high= 95)
+seismic_test = u.rescale_volume(u.get_cubes(name_test_dataset, test_data[4], 'seismic'), low= low_clip, high= high_clip)
 
 # Getting prediction on test data
 fault_predicted = np.zeros((300, 300, 1259))
@@ -25,12 +25,12 @@ for i in range(0, 300, 100):
         fault_predicted[i:i+100, j:j+100, :] = predict
 
 # Creation name files
-name_predicted = os.listdir(os.path.join('dark-side-test-data-part-3', test_data[4]))
+name_predicted = os.listdir(os.path.join(name_test_dataset, test_data[4]))
 name_predicted = re.sub(r'seismicCubes_RFC_fullstack_', 'fault_segments_', name_predicted[0])
 name_predicted = re.sub(r'.npy', '', name_predicted)
 name_predicted
 
-# 
+# Creation of .npz file with predictions of faults
 u.create_submission(sample_id=test_data[4], 
                     prediction=fault_predicted, 
                     submission_path=name_predicted, 
